dbad1a7968492ccf302cc423fb478f1bd8cc0fa1
Basem-Jamal <162180700+Basem-Jamal@users.noreply.github.com>
2025-05-15 08:21:37 +0300

    origin/main

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-    const containerList = document.querySelector("container")
-    const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box");
+
+inputBox.addEventListener("keydown" , e => { 
+        
+    if (e.key === "Enter") {
+        e.preventDefault();  // يمنع السلوك الافتراضي (مثلاً لو داخل فورم)
+        addTask();           // ينادي دالتك لإضافة المهمة
+    
+    }
+});
+
 
 function addTask () {
-    const containerList = document.querySelector("container")
-    const inputBox = document.getElementById("input-box");
 
-    if (containerList) {
-        let li = document.createElement("li");
-
-        li.innerHTML = inputBox.value;
-        containerList.appendChild(li);
+    if (inputBox.value === "") {
+        alert("عمي شلونك ؟.. لابد تكتب مهمة على الأقل");
 
     }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value ="";
 }
+
+listContainer.addEventListener("click",function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+},false);
