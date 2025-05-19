823299b9e9295f53d1ba069f0f55504bd6f7a0c7
Basem-Jamal <162180700+Basem-Jamal@users.noreply.github.com>
2025-05-19 19:01:11 +0300

    origin/main

---
+++ Notepad/note.js
@@ -0,0 +1,12 @@
+
+const listContainerNotes = document.getElementById("listOfNotes-container");
+
+const li = document.createElement("li");
+
+
+listContainerNotes.addEventListener('click' ,e =>{
+    
+})
+li.textContent ="Hi";
+
+listContainerNotes.appendChild("li");
